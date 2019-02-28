7a39c55fdc82209f3429bb4e9e59846c69f1db09
Amal T S <amalts0101@gmail.com>
2019-02-28 16:34:02 +0530

    Added Create Feature

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+# Post Controller
+class PostsController < ApplicationController
+  def new; end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  def show; end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+end
