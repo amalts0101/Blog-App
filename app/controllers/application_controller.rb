49cdb903e04fefa2189d898e57df0583b13e7c75
Amal T S <amalts0101@gmail.com>
2019-03-07 15:52:50 +0530

    Used latest hash syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 # Application controller
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, :except => [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 end
